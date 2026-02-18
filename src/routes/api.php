da527a523d12365d7f6b7daf1c07313ad5191c58
haruna <haruna0306@g.yju.ac.kr>
2026-02-18 18:51:50 +0900

    feat: APIルーティングを追加し各Controllerを接続

--- src/routes/api.php
+++ src/routes/api.php
@@ -3,6 +3,21 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+
+use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\DesignerController;
+use App\Http\Controllers\Api\NewsController;
+use App\Http\Controllers\Api\HairStyleController;
+use App\Http\Controllers\Api\ReservationController;
+use App\Http\Controllers\Api\TimeOffController;
+use App\Http\Controllers\Api\SalonController;
+use App\Http\Controllers\Api\ServiceController;
+
+Route::get('/service', [ServiceController::class, 'index']);
+
+
+
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
