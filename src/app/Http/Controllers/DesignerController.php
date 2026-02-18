ff74c7ebb5afd021369636ef372976af73740957
haruna <haruna0306@g.yju.ac.kr>
2026-02-18 18:24:04 +0900

    feat: 美容室予約システムの各種Controllerを作成

---
+++ src/app/Http/Controllers/DesignerController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Validation\Rule;
+use App\Models\Designer;
+use Illuminate\Http\Request;
+
+class DesignerController extends Controller
+{
+    /**
+     * すべてのデザイナー情報を表示する
+     */
+    public function index()
+    {
+        
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
