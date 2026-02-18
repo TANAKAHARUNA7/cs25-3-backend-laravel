9294c8c934073453f8918481a5e95496aa1c73a3
haruna <haruna0306@g.yju.ac.kr>
2026-02-18 20:42:17 +0900

    refactor(routes): api.phpのルーティング構成を整理

--- src/routes/api.php
+++ src/routes/api.php
@@ -4,16 +4,34 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
-use App\Http\Controllers\Api\UserController;
-use App\Http\Controllers\Api\DesignerController;
-use App\Http\Controllers\Api\NewsController;
-use App\Http\Controllers\Api\HairStyleController;
-use App\Http\Controllers\Api\ReservationController;
-use App\Http\Controllers\Api\TimeOffController;
-use App\Http\Controllers\Api\SalonController;
-use App\Http\Controllers\Api\ServiceController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\DesignerController;
+use App\Http\Controllers\NewsController;
+use App\Http\Controllers\HairStyleController;
+use App\Http\Controllers\ReservationController;
+use App\Http\Controllers\TimeOffController;
+use App\Http\Controllers\SalonController;
+use App\Http\Controllers\ServiceController;
+use App\Http\Controllers\AuthController;
 
-Route::get('/service', [ServiceController::class, 'index']);
+
+// 認証不要
+Route::post('login',[AuthController::class, 'login']); // ログイン
+Route::post('register',[AuthController::class, 'register']); // 会員登録
+
+// 認証必要
+Route::middleware('auth:sanctum')->group(function () {
+    // ログアウト
+    Route::post('logout', [AuthController::class, 'logout']);
+
+    // ログインユーザ情報摂取
+    Route::get('me', [AuthController::class, 'me']);
+
+});
+
+// サービスRouting
+// 認証不要
+Route::get(['services', ServiceController::class, 'index']);
 
 
 
