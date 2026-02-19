1896054fbddf1f42d53190f909919db3e78b5217
haruna <haruna0306@g.yju.ac.kr>
2026-02-19 22:18:13 +0900

    feat: ログイン機能およびロール別例外処理を実装

--- src/bootstrap/app.php
+++ src/bootstrap/app.php
@@ -12,8 +12,30 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->alias([
+            'role' => \App\Http\Middleware\RoleMiddleware::class,
+        ]);
     })
+    
+    // 例外（エラー）設定
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+
+        // 未認証エラー（AuthenticationException）が発生したときの処理
+        $exceptions->render(function(
+            // 未ログイン時に投げられる例外
+            Illuminate\Auth\AuthenticationException $e,
+            // 今のHTTPリクエスト
+            Illuminate\Http\Request $request
+        ){
+            // APIリクエスト（JSONを期待している場合）のみ
+            if ($request->expectsJson()) {
+                return response()->json([
+                    'success' => false,
+                    'error'   => [
+                        'code'    => 'UNAUTHORIZED',
+                        'message' => 'ログインが必要です.'
+                    ]
+                ], 401);
+            }
+        });
     })->create();
