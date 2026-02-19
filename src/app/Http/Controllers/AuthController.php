c934a8fefa5572f5da4a650246acd9e5299391c5
haruna <haruna0306@g.yju.ac.kr>
2026-02-19 11:37:22 +0900

    feat: 会員登録（register）ロジック実装

--- src/app/Http/Controllers/AuthController.php
+++ src/app/Http/Controllers/AuthController.php
@@ -7,10 +7,35 @@ use App\Models\User;
 use Illuminate\Http\JsonResponse;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rule;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class AuthController extends Controller
 {
+
+    public function register(Request $request): JsonResponse
+    {
+        $validated = $request->validate([
+            'account'   => ['required', 'string','max255'],
+            'password'  => ['required', 'string', 'max255'],
+            'user_name' => ['required', 'string', 'max255'],
+            'role'      => Rule::in(['client', 'designer', 'manager']),
+            'gender'    => Rule::in(['MEN','WOMEN', 'Non-binary']),
+            'phone'     => ['string'],
+            'birth'     => ['required', 'string'],
+        ]);
+
+        DB::table('users')->insert($validated);
+
+        return response()->json([
+            'success' => true,
+            'message' => '会員登録完了しました'
+        ]);
+    }
+
+
+
     public function login(Request $request): JsonResponse
     {
         $validated = $request->validate([
