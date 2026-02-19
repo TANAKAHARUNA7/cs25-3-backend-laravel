849909b15f76606d573b9ad85e417d5988a9e970
haruna <haruna0306@g.yju.ac.kr>
2026-02-19 12:28:45 +0900

    fix(auth): registerのバリデーションとパスワードハッシュ処理を修正

--- src/app/Http/Controllers/AuthController.php
+++ src/app/Http/Controllers/AuthController.php
@@ -9,29 +9,33 @@ use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rule;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 
 class AuthController extends Controller
 {
 
     public function register(Request $request): JsonResponse
     {
+        // バリデーションチェック
         $validated = $request->validate([
-            'account'   => ['required', 'string','max255'],
-            'password'  => ['required', 'string', 'max255'],
-            'user_name' => ['required', 'string', 'max255'],
-            'role'      => Rule::in(['client', 'designer', 'manager']),
-            'gender'    => Rule::in(['MEN','WOMEN', 'Non-binary']),
-            'phone'     => ['string'],
-            'birth'     => ['required', 'string'],
+            'account'   => ['required', 'string','max:255', 'unique:users,account'],
+            'password'  => ['required', 'string', 'max:255'],
+            'user_name' => ['required', 'string', 'max:255'],
+            'role'      => ['required', Rule::in(['client', 'designer', 'manager'])],
+            'gender'    => ['required', Rule::in(['MEN','WOMEN', 'Non-binary'])],
+            'phone'     => ['nullable', 'string', 'max:30'],
+            'birth'     => ['required', 'date'],
         ]);
 
-        DB::table('users')->insert($validated);
+        // パスワードハッシュ処理
+        $validated['password'] = Hash::make($validated['password']);
+
+        // UserModelを使ってDBへデータをINSERT 
+        User::create($validated);
 
         return response()->json([
             'success' => true,
             'message' => '会員登録完了しました'
-        ]);
+        ], 201);
     }
 
 
