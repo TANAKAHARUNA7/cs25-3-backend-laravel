805c92577776f7226dfcbafd8b39c12730d251ff
haruna <haruna0306@g.yju.ac.kr>
2026-02-20 15:17:18 +0900

    feat: デザイナー休日新規作成実装途中

--- src/app/Http/Controllers/TimeOffController.php
+++ src/app/Http/Controllers/TimeOffController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Models\TimeOff;
+use App\Models\Designer;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Validation\Rule;
 use Illuminate\Http\Request;
@@ -16,18 +17,29 @@ class TimeOffController extends Controller
     public function index(): JsonResponse
     {
         // DB接続しデータを摂取
-        $result = TimeOff::all();
+        $timeOffs = TimeOff::all();
         
         // フロントに返す
-        return response()->json($result);
+        return response()->json($timeOffs);
     }
 
     /**
-     * Store a newly created resource in storage.
+     * 特定のDesignerの休日を新規作成
      */
-    public function store(Request $request)
+    public function store(Request $request): JsonResponse
     {
-        //
+
+        $designer = TimeOff::designer()->findOrFail();
+
+        $request->validate([
+            'designer_id' => ['required', $designer],
+            'start_at'    => ['required', 'date'],
+            'end_at'      => ['required', 'date']
+        ]);
+
+        TimeOff::created($request);
+
+        return response()->json();
     }
 
     /**
