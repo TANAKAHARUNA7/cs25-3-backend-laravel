1623f07a66fe0a1e8e254f55d717f57d7a6ca432
haruna <haruna0306@g.yju.ac.kr>
2026-03-07 18:58:18 +0900

    fix: designer-controller修正

--- src/app/Http/Controllers/DesignerController.php
+++ src/app/Http/Controllers/DesignerController.php
@@ -17,7 +17,7 @@ class DesignerController extends Controller
     public function index():JsonResponse
     {
         $designer = Designer::all();
-        
+
         return response()->json([
             'success' => true,
             'data'    => $designer
@@ -42,17 +42,21 @@ class DesignerController extends Controller
         ]);
 
         // DBに保存する値
-        return DB::transaction(function () use ($request, $imageService, $validated, $userId) {
-            
+        return DB::transaction(function () use ($request, $imageService, $validated, $userId):JsonResponse {
+
             //　画像保存
             $img = $imageService->store($request->file('image'), 'designer');
 
             //　DBに保存する値をまとめる
-            $data = array_merge($validated, [
-                'user_id'   => $userId,
-                'image_key' => $img['image_key'],
-                'image'     => $img['image'], 
-            ]);
+            $data =[
+                'user_id'     => $userId,
+                'experience'  => $validated['experience'],
+                'good_at'     => $validated['good_at'],
+                'personality' => $validated['personality'],
+                'message'     => $validated['message'],
+                'image_key'   => $img['image_key'],
+                'image'       => $img['image'],
+            ];
 
             Designer::create($data);
 
@@ -80,20 +84,20 @@ class DesignerController extends Controller
     /**
      * 自分のプロフィールのみ編集可能
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, ImageService $imageService): JsonResponse
     {
         $userId = $request->user()->id;
 
         $validated = $request->validate([
-            'image'       => ['required', 'image', 'max:2048'],
+            'image'       => ['nullable', 'image', 'max:2048'],
             'experience'  => ['required', 'integer'],
             'good_at'     => ['required', 'string'],
             'personality' => ['required', 'string'],
-            'message'     => ['required', 'string'], 
+            'message'     => ['required', 'string'],
         ]);
 
         return DB::transaction(function () use ($request, $imageService, $validated, $userId) {
-            
+
             // 本人のDesignerレコードを取得（無ければ404）
             $designer = Designer::where('user_id', $userId)->firstOrFail();
 
@@ -101,17 +105,21 @@ class DesignerController extends Controller
 
             // 画像が送られてきたときだけ保存してDB更新
             if ($request->hasFile('image')) {
-                
+
+                $imageService->delete($designer->image_key);
+
                 $img = $imageService->store($request->file('image'), 'designer');
 
                 $data['image_key'] = $img['image_key'];
-                $data['immage']    = $img['image'];
+                $data['image']    = $img['image'];
+            } else {
+                // DBに保存できないので消す（nullableでも来た場合UploadedFileになる）
+                unset($data['image']);
             }
 
-            // DBに保存できないので消す（nullableでも来た場合UploadedFileになる）
-            unset($data['image']);
+            $designer->update($data);
 
-            $designer->update()->json([
+            return response()->json([
                 'success' => true,
                 'message' => '更新に成功しました。'
             ], 200);
@@ -119,10 +127,21 @@ class DesignerController extends Controller
     }
 
     /**
-     * Remove the specified resource from storage.
+     * managerのみdesignerのプロフィールを削除可能
      */
-    public function destroy(string $id)
+    public function destroy(string $id, ImageService $imageService): JsonResponse
     {
-        //
+        // 該当するdesignerの情報を持ってくる
+        $designer = Designer::findOrFail($id);
+
+        // 画像ファイル削除
+        $imageService->delete($designer->image_key);
+
+        // レコード削除
+        Designer::destroy($id);
+
+        return response()->json([
+            'success'=> true
+        ]);
     }
 }
