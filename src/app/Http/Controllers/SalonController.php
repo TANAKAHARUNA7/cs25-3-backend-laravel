7992af03fd8c127de767816e1fe0ae6bea118af9
haruna <haruna0306@g.yju.ac.kr>
2026-03-07 18:40:24 +0900

    feat: salonコントローラー実装

--- src/app/Http/Controllers/SalonController.php
+++ src/app/Http/Controllers/SalonController.php
@@ -3,48 +3,72 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Validation\Rule;
+use App\Models\Salon;
+use App\Services\ImageService;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class SalonController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * サロン情報表示
      */
-    public function index()
+    public function index():JsonResponse
     {
-        //
+        $salon = Salon::all();
+
+        return response()->json([
+            'saccess' => true,
+            'data'    => $salon
+        ]);
     }
 
     /**
-     * Store a newly created resource in storage.
+     * サロン情報update
      */
-    public function store(Request $request)
+    public function update(Request $request, ImageService $imageService) :JsonResponse
     {
-        //
+        $validated = $request->validate([
+            'image'        => ['nullable', 'image', 'max:2048'],
+            'introduction' => ['required', 'string'],
+            'information'  => ['required', 'json'],
+            'map'          => ['required', 'string'],
+            'traffic'      => ['required', 'json'],
+        ]);
+
+        // DBに保存する値
+        return DB::transaction(function () use ($request, $validated, $imageService) {
+
+            $salon = Salon::firstOrFail();
+
+            $data = [
+                'introduction' => $validated['introduction'],
+                'information'  => json_decode($validated['information'], true),
+                'map'          => $validated['map'],
+                'traffic'      => json_decode($validated['traffic'], true),
+            ];
+
+            if ($request->hasFile('image')) {
+
+                // 既存画像削除
+                $imageService->delete($salon->image_key);
+
+                // 新画像を保存
+                $img = $imageService->store($request->file('image'), 'salon');
+
+                $data['image']     = $img['image'];
+                $data['image_key'] = $img['image_key'];
+            }
+
+            $salon->update($data);
+
+            return response()->json([
+                'success' => true,
+                'message' => '修正しました。'
+            ], 201);
+
+        });
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
-    }
 }
